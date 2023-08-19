eb60c5b79b399400c660f2df1d6d33e436b24e5b
ehdtls123 <wlswls9964@naver.com>
2023-08-19 19:38:57 +0900

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,32 @@
-import { useState } from 'react'
+import "./App.scss";
+import { useState } from "react";
+import SwiperCore, { Navigation, Pagination, Autoplay } from "swiper";
+import "swiper/swiper.scss";
+import "swiper/components/navigation/navigation.scss";
+import "swiper/components/pagination/pagination.scss";
+import testImg from "/img/download.jpg";
 
-import './App.css'
+const icon = [
+    {
+        img: "a",
+        title: "직잭뷰티",
+    },
+    {
+        img: "a",
+        title: "브랜드패션",
+    },
+    {
+        img: "a",
+        title: "상의",
+    },
+    {
+        img: "a",
+        title: "원피스",
+    },
+];
 
-function App() {
-  
+const App = () => {
+    return <div className="App"></div>;
+};
 
-  return (
-   <div className='main'>
-    <div>
-      
-    </div>
-
-
-   </div>
-  )
-}
-
-export default App
+export default App;
