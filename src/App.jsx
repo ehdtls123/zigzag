d59c6600333fbe4d85d5f2fca04467339da72961
jin <hyunjin9964@gmail.com>
2023-08-23 00:13:55 +0900

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,45 @@
-import "./App.scss";
-import Header from "./Components/Header/Header";
-import Footer from "./Components/Footer/Footer";
-import { Route, Routes } from "react-router-dom";
-import Home from "./Pages/Home/Home";
-import Categories from "./Pages/Categories/Categories";
-import Picks from "./Pages/Picks/Picks";
-import MyPage from "./Pages/MyPage/MyPage";
+import './App.scss';
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import Categories from './Pages/Categories/Categories';
+import Picks from './Pages/Picks/Picks';
+import MyPage from './Pages/MyPage/MyPage';
 
 const App = () => {
-    const pages = [
-        {
-            path: "/",
-            component: <Home />,
-        },
-        {
-            path: "/categories",
-            component: <Categories />,
-        },
-        {
-            path: "/picks",
-            component: <Picks />,
-        },
-        {
-            path: "/myPage",
-            component: <MyPage />,
-        },
-    ];
+	const pages = [
+		{
+			path: '/',
+			component: <Home />,
+		},
+		{
+			path: '/categories',
+			component: <Categories />,
+		},
+		{
+			path: '/picks',
+			component: <Picks />,
+		},
+		{
+			path: '/myPage',
+			component: <MyPage />,
+		},
+	];
 
-    return (
-        <div className="App">
-            <Header />
-            <div className="app-contents">
-                <Routes>
-                    {pages.map((v) => {
-                        return (
-                            <Route
-                                key={v.path}
-                                path={v.path}
-                                element={v.component}
-                            />
-                        );
-                    })}
-                </Routes>
-            </div>
-            <Footer />
-        </div>
-    );
+	return (
+		<div className='App'>
+			<Header />
+			<div className='app-contents'>
+				<Routes>
+					{pages.map(v => {
+						return <Route key={v.path} path={v.path} element={v.component} />;
+					})}
+				</Routes>
+			</div>
+			<Footer />
+		</div>
+	);
 };
 
 export default App;
